d683a46ffda27a0fd37cae8ca69dd606251f5e5b
Jivak Sute <jivakysute@gmail.com>
2023-06-14 23:22:40 +0530

    Day-2

---
+++ backend/google.auth.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+require("./auth");
+
+const app = express();
+require("dotenv").config();
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  res.send('<a href="/auth/google">Sign up with Google</a>');
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+
+app.get("/protected", isLoggedIn, (req, res) => {
+  console.log("user", req.user);
+  res.send(`Hello ${req.user.displayName}`);
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send("Goodbye!");
+});
+
+app.get("/auth/google/failure", (req, res) => {
+  res.send("Failed to authenticate..");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port: ${process.env.PORT}`)
+);
