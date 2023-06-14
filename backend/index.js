4b3e7ee2f41826acffff4a2fafd513970630be2f
harpreet-1 <hpsllg8@gmail.com>
2023-06-14 19:19:36 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./server");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  connection();
+});
