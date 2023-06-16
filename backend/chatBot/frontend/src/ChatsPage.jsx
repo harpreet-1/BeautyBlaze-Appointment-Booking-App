358bb7d48818d4da123773481a0a8e106db99e9b
VishalDnyandeoPatil <vp1610858@gmail.com>
2023-06-17 01:07:52 +0530

    Day4 work

---
+++ backend/chatBot/frontend/src/ChatsPage.jsx
@@ -0,0 +1,24 @@
+// import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    // const chatProps = useMultiChatLogic(
+    //     '40720d81-239d-44c5-8379-525a0b5c1fa5',
+    //     props.user.username,
+    //     props.user.secret
+    // );
+    return (
+        <div style={{ height: '100vh' }}>
+            {/* <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} /> */}
+            <PrettyChatWindow
+                projectId='40720d81-239d-44c5-8379-525a0b5c1fa5'
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%' }}
+            />
+        </div>
+    )
+}
+
+export default ChatsPage
