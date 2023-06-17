7139436d2432335c304128adbe51c560a622bb77
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-06-17 23:12:38 +0530

    day-5 navbar and professional registration done

---
+++ client/src/pages/Professional.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfessionalComponent from "../components/professional/ProfessionalComponent";
+
+function Professional() {
+  return (
+    <>
+      <ProfessionalComponent />
+    </>
+  );
+}
+
+export default Professional;
