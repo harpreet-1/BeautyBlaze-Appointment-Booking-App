da054962f45e96b650eedaa8c672696d6ea7c674
harpreet-1 <hpsllg8@gmail.com>
2023-09-01 19:24:52 +0530

    responsive and booking page done

--- client/src/pages/SharedLayout.jsx
+++ client/src/pages/SharedLayout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../components/home/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/home/Footer";
+import NavScrollExample from "../components/home/NavScrollExample";
 
 const SharedLayout = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      <NavScrollExample />
       <Outlet />
       <Footer />
     </>
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,52 +1,97 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { signup } from "../../db";
-const url = "https://pear-splendid-bee.cyclic.app/appointment/book";
-
+// const url = "https://pear-splendid-bee.cyclic.app/appointment/book";
+const url = "http://localhost:8000/appointment/book";
+import "../Styling/bookslot.css";
+import SuccessToast from "../components/toast/SuccessToast";
+import ErrorToast from "../components/toast/ErrorToast";
 const Single = () => {
+  const navigate = useNavigate();
+  const [tostMessage, setTostMessage] = useState(null);
+  const [errorTostMessage, setErrorTostMessage] = useState(null);
+  let userId = JSON.parse(localStorage.getItem("id")) || null;
+
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const { id, professionalId } = useParams();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(date, time);
+    if (!userId) {
+      setErrorTostMessage("Please Login To book Slot");
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000);
+      return;
+    }
+
     try {
-      const data = await signup(url, {
-        customerID: localStorage.getItem("id"),
+      const Slotdata = {
+        customerID: userId,
         service: id,
         beautyProfessionalID: professionalId,
         date: date,
         time: time,
         notes: "appointment booked",
+      };
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify(Slotdata),
       });
+      const data = await response.json();
+
       console.log(data);
-      // if (data.token) {
-      //   navigation("/");
-      // }
+
+      if (data.success) {
+        setTostMessage("Appointment Booked Successfully");
+        setTimeout(() => {
+          navigate("/");
+        }, 3000);
+      }
+      setErrorTostMessage(data.message);
+      setTimeout(() => {
+        setErrorTostMessage(null);
+      }, 3000);
     } catch (error) {
       console.error("Error:", error);
     }
   };
   return (
     <div>
-      <p>single page</p>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <select onChange={(e) => setTime(e.target.value)}>
-          <option value="Choose slot">Choose slot</option>
+      {tostMessage && <SuccessToast message={tostMessage} />}
+      {errorTostMessage && <ErrorToast message={errorTostMessage} />}
+      <h2 className="bookheading">Book Your Slot Here</h2>
+      <form className="bookingForm" onSubmit={(e) => handleSubmit(e)}>
+        <select
+          required
+          className="form-control"
+          onChange={(e) => setTime(e.target.value)}
+        >
+          <option value="">Choose slot</option>
           <option value="10:00 AM">10:00 AM</option>
           <option value="11:00 AM">11:00 AM</option>
           <option value="1:00 AM">1:00 PM</option>
           <option value="2:00 AM">2:00 AM</option>
           <option value="3:00 AM">3:00 AM</option>
         </select>
-
+        <label> Select Date</label>
         <input
+          required
+          className="form-control"
           type="date"
           value={date}
           onChange={(e) => setDate(e.target.value)}
         />
-        <input type="submit" value="Book Appointment" />
+        <input
+          className="form-control"
+          type="submit"
+          value="Book Appointment"
+        />
       </form>
     </div>
   );
